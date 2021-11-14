fe1af08f5fa7c467df9a006dfe6ab0ac7f3c90e3
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-14 19:28:25 +0100

    Test for usersController was added

--- server/middlewares/createUser.js
+++ server/middlewares/createUser.js
@@ -2,14 +2,16 @@ const bcrypt = require("bcrypt");
 const User = require("../../database/models/user");
 
 const createUser = async (req, res, next) => {
-  const { name, username, password } = req.body;
+  const { name, username, password, age, image = req.file } = req.body;
   try {
     const user = await User.create({
       name,
       username,
       password: await bcrypt.hash(password, 10),
+      age,
+      image,
     });
-    res.json(user);
+    res.status(201).json(user);
   } catch {
     const error = new Error("Bad credentials provided");
     error.code = 400;
